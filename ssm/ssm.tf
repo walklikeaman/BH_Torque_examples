3186ad33571c33766691e3359342d919efbf9240
bhandley <brian.handley@alum.mit.edu>
2023-05-23 22:46:44 -0400

    Create ssm.tf
    Create ssm parameter with variable name and value

---
+++ ssm/ssm.tf
@@ -0,0 +1,46 @@
+
+provider "aws" {
+  region  = var.aws_region
+
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">3.0.0"
+    }
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "name" {
+  type    = string
+  default = "test_name"
+}
+
+variable "value" {
+  type    = string
+  default = "test_value"
+  sensitive = true
+}
+
+resource "aws_ssm_parameter" "foo" {
+  name  = "name-${var.name}"
+  # type  = "SecureString"
+  type  = "String"
+  value = "value-${var.value}"
+  tags = {
+    environment = "update"
+  }
+}
+
+output "ssm_value" {
+  value = aws_ssm_parameter.foo.value
+  sensitive = true
+}
+
